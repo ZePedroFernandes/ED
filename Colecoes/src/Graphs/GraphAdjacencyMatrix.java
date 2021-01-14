c012a426323d7594e7d35a11d69fc71fe55eac2b
José Fernandes <8190239@estg.ipp.pt>
2021-01-14 12:17:48 +0000

    Create GraphAdjacencyMatrix.java

---
+++ Colecoes/src/Graphs/GraphAdjacencyMatrix.java
@@ -0,0 +1,66 @@
+package Graphs;
+
+import ADTs.GraphADT;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Nome : José Pedro Fernandes Número: 8190239 Turma: 1
+ */
+public class GraphAdjacencyMatrix<T> implements GraphADT<T> {
+
+    @Override
+    public void addVertex(T vertex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void removeVertex(T vertex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void addEdge(T vertex1, T vertex2) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void removeEdge(T vertex1, T vertex2) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Iterator iteratorBFS(T startVertex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Iterator iteratorDFS(T startVertex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Iterator iteratorShortestPath(T startVertex, T targetVertex) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isConnected() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
