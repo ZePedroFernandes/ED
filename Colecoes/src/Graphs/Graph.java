a53d01978ca4d84a8ee4bb89cff4446adb30cbf3
José Fernandes <8190239@estg.ipp.pt>
2021-01-14 14:16:51 +0000

    commit

--- Colecoes/src/Graphs/Graph.java
+++ Colecoes/src/Graphs/Graph.java
@@ -15,27 +15,97 @@ public class Graph<T> implements GraphADT<T> {
     protected T[] vertices;   // values of vertices
 
     /**
-     * * Creates an empty graph.
+     * Creates an empty graph.
      */
-    public Graph {
+    public Graph() {
         numVertices = 0;
-        this.adjMatrix = new boolean[DEFAULT_CAPACITY][DEFAULT_CAPACITY];
-        this.vertices = (T[]) (new Object[DEFAULT_CAPACITY]);
+        adjMatrix = new boolean[DEFAULT_CAPACITY][DEFAULT_CAPACITY];
+        vertices = (T[]) (new Object[DEFAULT_CAPACITY]);
     }
 
-    @Override
-    public void addVertex(T vertex) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private boolean indexIsValid(int index) {
+        return ((index < numVertices) && (index >= 0));
     }
 
-    @Override
-    public void removeVertex(T vertex) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private int getIndex(T targetVertex) {
+        for (int i = 0; i < numVertices; i++) {
+            if (this.vertices[i].equals(targetVertex)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void addEdge(int index1, int index2) {
+        if (indexIsValid(index1) && indexIsValid(index2)) {
+            adjMatrix[index1][index2] = true;
+            adjMatrix[index2][index1] = true;
+        }
     }
 
     @Override
     public void addEdge(T vertex1, T vertex2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        addEdge(getIndex(vertex1), getIndex(vertex2));
+    }
+
+    private void expandCapacity() {
+        T[] expandedVertices = (T[]) new Object[vertices.length * 2];
+        boolean[][] expandedMatrix = new boolean[vertices.length * 2][vertices.length * 2];
+
+        for (int i = 0; i < vertices.length; i++) {
+
+            for (int j = 0; j < vertices.length; j++) {
+                expandedMatrix[i][j] = adjMatrix[i][j];
+            }
+
+            expandedVertices[i] = vertices[i];
+        }
+        vertices = expandedVertices;
+        adjMatrix = expandedMatrix;
+    }
+
+    @Override
+    public void addVertex(T vertex) {
+        if (numVertices == vertices.length) {
+            expandCapacity();
+        }
+        vertices[numVertices] = vertex;
+        for (int i = 0; i <= numVertices; i++) {
+            adjMatrix[numVertices][i] = false;
+            adjMatrix[i][numVertices] = false;
+        }
+        numVertices++;
+    }
+
+    @Override
+    public void removeVertex(T vertex) {
+        removeVertex(getIndex(vertex));
+    }
+
+    public void removeVertex(int index) {
+        if (indexIsValid(index)) {
+            this.numVertices--;
+
+            for (int i = index; i < this.numVertices; i++) {
+                this.vertices[i] = this.vertices[i + 1];
+            }
+
+            for (int i = index; i < this.numVertices; i++) {
+                for (int j = 0; j <= this.numVertices; j++) {
+                    adjMatrix[i][j] = adjMatrix[i + 1][j];
+                }
+            }
+            //Lines shifted
+
+            for (int i = index; i < this.numVertices; i++) {
+                for (int j = 0; j < this.numVertices; j++) {
+                    adjMatrix[j][i] = adjMatrix[j][i + 1];
+                }
+            }
+            //Columes shifted
+
+        }
     }
 
     @Override
--- Colecoes/src/zmain/main.java
+++ Colecoes/src/zmain/main.java
@@ -1,13 +1,7 @@
 package zmain;
 
-import exceptions.ElementNotFoundException;
-import java.util.Iterator;
-import trees.AVLTree;
-import trees.ArrayHeap;
-import trees.HeapNode;
-import trees.priorityQueues.PriorityQueue;
-import trees.priorityQueues.PriorityQueueNode;
-import trees.TreePrinter;
+import Graphs.Graph;
+import java.util.Objects;
 
 /**
  *
@@ -18,52 +12,72 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ElementNotFoundException {
-//        ArrayHeap<Integer> heap = new ArrayHeap<>();
-//
-//        heap.addElement(-10);
-//        heap.addElement(-1);
-//        heap.addElement(0);
-//        heap.addElement(1);
-//        heap.addElement(7);
-//        heap.addElement(9);
-//        heap.addElement(1);
-//        heap.addElement(2);
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
-//
-//        heap.removeMin();
-//
-//        TreePrinter.printLevelOrder(heap);
+    public static void main(String[] args) {
+        Graph<Vertex> graph = new Graph();
+        Vertex vertex1 = new Vertex("A", 3);
+        Vertex vertex2 = new Vertex("B", 3);
+        Vertex vertex3 = new Vertex("C", 3);
+        Vertex vertex4 = new Vertex("D", 3);
+        Vertex vertex5 = new Vertex("E", 3);
+        Vertex vertex6 = new Vertex("F", 3);
+        Vertex vertex7 = new Vertex("G", 3);
+        Vertex vertex8 = new Vertex("H", 3);
+        Vertex vertex9 = new Vertex("I", 3);
+        Vertex vertex10 = new Vertex("J", 3);
+        
+        graph.addVertex(vertex1);
+        graph.addVertex(vertex2);
+        graph.addVertex(vertex3);
+        graph.addVertex(vertex4);
+        graph.addVertex(vertex5);
+        graph.addVertex(vertex6);
+        graph.addVertex(vertex7);
+        graph.addVertex(vertex8);
+        graph.addVertex(vertex9);
+        graph.addVertex(vertex10);
 
+        System.out.println("");
+        
+        graph.removeVertex(vertex3);
+        
+        System.out.println("");
     }//Main
 
+}
+
+class Vertex {
+
+    String name;
+
+    int info;
+
+    public Vertex() {
+
+    }
+
+    public Vertex(String name, int info) {
+        this.name = name;
+        this.info = info;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vertex other = (Vertex) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
